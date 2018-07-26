18b100c6292900fcafa32952214e59bb94833e1a
Yurina Shin <yurinas@gmail.com>
2018-07-26 18:58:52 +0000

    automatically backed up by learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -13,7 +13,6 @@ class Genre < ActiveRecord::Base
   end
 
   def all_artist_names
-    binding.pry
     self.artists.map{|a| a.name}
     # return an array of strings containing every musician's name
   end
